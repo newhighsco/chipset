69e0196b843ad343589afc60990035e8acc63a9b
Ben Edwards <hello@benedfit.com>
2020-03-15 12:52:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chipset
+
+## [CHANGELOG](CHANGELOG.md)
---
+++ index.js
@@ -0,0 +1,4 @@
+const path = require('path')
+
+export const includePaths = [path.join(__dirname, 'src/styles')]
+export * from './components'
