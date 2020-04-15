0c369d122d15e348de9f885932a8bc660ababd99
Ben Edwards <hello@benedfit.com>
2020-04-15 11:46:45 +0100

    test(element): add element component tests

---
+++ src/components/Element/index.spec.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Element } from '.'
+
+describe('Components/Element', () => {
+  it('should render <div /> by default', () => {
+    const wrapper = shallow(<Element />)
+
+    expect(wrapper.type()).toEqual('div')
+  })
+
+  it("should render the correct component when 'as' is set", () => {
+    const wrapper = shallow(<Element as="span" />)
+
+    expect(wrapper.type()).toEqual('span')
+  })
+})
