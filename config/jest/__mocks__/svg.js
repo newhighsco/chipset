e5e1837c2eabf453af8cbf0de92e22613378a6d6
Ben Edwards <hello@benedfit.com>
2020-04-15 10:50:50 +0100

    test: handle non-js files in jest

---
+++ config/jest/__mocks__/svg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MockSvg = () => (
+  <svg viewBox="0 0 24 24">
+    <path d="M0 0h48v1H0z" />
+  </svg>
+)
+
+export default MockSvg
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    '\\.(scss)$': 'identity-obj-proxy',
+    '\\.(svg)$': './config/jest/__mocks__/svg.js',
+    '\\.(png|jpg|gif|eot|ttf|woff(2)?)$': './config/jest/file-transform.js'
+  },
   setupFiles: ['./config/jest/setup-tests.js']
 }
