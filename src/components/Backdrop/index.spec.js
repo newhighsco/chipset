9f6252c41483ed70b544ecc917d7f38b71ae027d
Ben Edwards <hello@benedfit.com>
2020-08-04 15:55:55 +0100

    test(backdrop): fixes typo

--- src/components/Backdrop/index.spec.js
+++ src/components/Backdrop/index.spec.js
@@ -20,12 +20,12 @@ describe('Components/Backdrop', () => {
   })
 
   it("should render a <video /> when 'video' is set", () => {
-    const wrapper = shallow(<Backdrop video="https://example.com/image.mp4" />)
+    const wrapper = shallow(<Backdrop video="https://example.com/video.mp4" />)
 
     expect(wrapper.type()).toEqual(Element)
     expect(wrapper.prop('style')).toEqual(undefined)
     expect(wrapper.prop('as')).toEqual('video')
-    expect(wrapper.prop('src')).toEqual('https://example.com/image.mp4')
+    expect(wrapper.prop('src')).toEqual('https://example.com/video.mp4')
     expect(wrapper.prop('loop')).toEqual(true)
   })
 })
