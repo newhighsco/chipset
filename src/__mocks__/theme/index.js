d85da853b8fe93dbf97a43972cf1530c7a31856c
Ben Edwards <hello@benedfit.com>
2020-08-20 13:54:10 +0100

    chore: adds mock styles for List and Prose

--- src/__mocks__/theme/index.js
+++ src/__mocks__/theme/index.js
@@ -3,8 +3,10 @@ import Card from '../styles/components/Card.module.scss'
 import ContentContainer from '../styles/components/ContentContainer.module.scss'
 import CreditLockup from '../styles/components/CreditLockup.module.scss'
 import Grid from '../styles/components/Grid.module.scss'
+import List from '../styles/components/List.module.scss'
 import Navigation from '../styles/components/Navigation.module.scss'
 import PageContainer from '../styles/components/PageContainer.module.scss'
+import Prose from '../styles/components/Prose.module.scss'
 import SiteContainer from '../styles/components/SiteContainer.module.scss'
 
 export default {
@@ -13,7 +15,9 @@ export default {
   ContentContainer,
   CreditLockup,
   Grid,
+  List,
   Navigation,
   PageContainer,
+  Prose,
   SiteContainer
 }
