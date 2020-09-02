33b311cc87925dbd57cf6ca51dccc7b2e18cb3bb
Ben Edwards <hello@benedfit.com>
2020-09-02 13:51:20 +0100

    docs: move storybook README

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../src/index.stories.mdx', '../src/**/*.stories.@(js|mdx)'],
+  stories: ['./README.stories.mdx', '../src/**/*.stories.@(js|mdx)'],
   addons: ['@newhighsco/storybook-preset']
 }
