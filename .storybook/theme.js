119e5e21f4ec162957967519d66892ff40a8fdb9
Ben Edwards <hello@benedfit.com>
2024-04-10 14:58:33 +0100

    chore: update description

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,12 +1,8 @@
 import { create } from '@storybook/theming/create'
-import packageInfo from '../package.json'
-
-const { config, homepage, name } = packageInfo
-const { logo, theme, title } = config
 
 export default create({
-  base: theme,
-  brandTitle: title || name,
-  brandUrl: homepage,
-  brandImage: logo
+  base: 'dark',
+  brandTitle: 'Chipset',
+  brandUrl: '/',
+  brandImage: '/logo.svg'
 })
