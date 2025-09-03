b2afee106e862a3b7c0005e5ecba0d13066293da
Ben Edwards <hello@benedfit.com>
2025-09-03 15:23:19 +0100

    chore(renovate): move config to file

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { ThemeProvider } from '../src/providers'
-import componentTheme from '../example/theme'
-
-import breakpoints from '../styles/_breakpoints.module.scss'
 import './preview.scss'
+
+import React from 'react'
+
+import componentTheme from '../example/theme'
+import { ThemeProvider } from '../src/providers'
+import breakpoints from '../styles/_breakpoints.module.scss'
 
 const viewports = [Number(breakpoints.desktopLarge)]
 
