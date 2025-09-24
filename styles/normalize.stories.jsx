f2f3eb408ddcb321990c9ef84e5cf2bbaa8b7b21
Ben Edwards <hello@benedfit.com>
2025-09-24 13:42:22 +0100

    test(normalize): baseline (#2714)

---
+++ styles/normalize.stories.jsx
@@ -0,0 +1,17 @@
+import { KitchenSink } from '../src/__mocks__/fixtures'
+
+export default { component: KitchenSink }
+
+export const Source = {
+  parameters: {
+    chromatic: { disable: true },
+    docs: {
+      source: {
+        code: "@use '~@newhighsco/chipset/styles/normalize';",
+        language: 'css'
+      }
+    }
+  }
+}
+
+export const Example = {}
