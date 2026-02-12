e9125dd3cc83ff9aa9a243a11cb931bb7615476f
Ben Edwards <hello@benedfit.com>
2026-02-12 16:47:41 +0000

    chore: typo

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,14 @@
-module.exports = {
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
   framework: '@storybook/react-webpack5',
   stories: ['../src', { directory: '../styles', titlePrefix: 'Styles' }],
   addons: ['@newhighsco/storybook-preset'],
-  staticDirs: ['./static']
+  staticDirs: ['./static'],
+  webpackFinal: config => {
+    config.resolve.alias['@newhighsco/chipset'] = process.cwd()
+
+    return config
+  }
 }
+
+export default config
